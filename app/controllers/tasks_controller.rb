0e1028bcdfefd01b8ee4998344adb0c6812160ff
高瀬康平 <takaseko@takasekouheinoMacBook-Pro.local>
2020-06-13 04:43:01 +0900

    詳細表示実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
 
   # 一覧表示するためのアクション
   def index
-    @tasks = Task.all
+    @tasks= Task.all
   end
 
   # 新規登録するためのアクション
@@ -13,6 +13,11 @@ class TasksController < ApplicationController
   # 更新するためのアクション
   def create
     Task.create(task_params)
+  end
+
+  # 詳細を表示するためのアクション
+  def show
+    @task = Task.find(params[:id])
   end
 
   # データベースに登録するデータを制限するためのParameterStrongの記載
