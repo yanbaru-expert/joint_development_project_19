3bb01255979e69220b842ef8125c5b29b57d5a64
Takuma Tajikawa <takumatajikawa@TakumanoMacBook-Pro.local>
2020-06-16 06:18:47 +0900

    routesのHEADとmasterを削除しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
- HEAD
- HEAD
   resources :users
 end
 
   # resourcesを使用したルーティング
   resources  :tasks
 end
-master
 
   resources :messages
   #resourceを使用したルーティング
   resources :tasks
 end
-master
+
